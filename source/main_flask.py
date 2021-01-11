77a135e24124c16d3c3bc01a0d56e00e0dba0f09
daumhch <daumhch@naver.com>
2021-01-11 16:52:38 +0900

    플라스크 업데이트 확인 및 구름IDE에 올려보기

--- source/main_flask.py
+++ source/main_flask.py
@@ -1,17 +1,22 @@
 from flask import Flask
 from apscheduler.schedulers.background import BackgroundScheduler
+import datetime
+from read_schedule import read_schedule
 
 app = Flask(__name__)
 
-
+GLOBAL_COUNT = 0
 
 # html 파일 연결하기
 from flask import render_template
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", data=GLOBAL_COUNT)
 
 def schedule_job():
+    global GLOBAL_COUNT
+    GLOBAL_COUNT = GLOBAL_COUNT+1
+    print('GROVAL_COUNT:',GLOBAL_COUNT)
     print('I am working...')
 
 if __name__ == '__main__':
--- source/read_schedule.py
+++ source/read_schedule.py
@@ -1,29 +1,32 @@
 # 다시만들어야 한다 왜냐면 2일치를 붙여도 마지막을 읽기 때문에
-import time
 import numpy as np
+import pandas as pd
 
 from datetime import datetime, timedelta
 import requests
 from bs4 import BeautifulSoup
 
+
 def read_Soup(path, day):
-    return BeautifulSoup(requests.get(path+day).text, 'html5lib')
+    return BeautifulSoup(requests.get(path+day).text, 'html.parser')
+
 
 def cut_HTML(save, soup, now_day, tomo_day, option):
     if option == 'TIME':
         temp = soup.select('td.programInfo em')
         for i, value in enumerate(temp):
-            j = value.text.replace('\t','').replace(' ','').replace('\n','')
-            if i>len(temp)/2 and j.startswith('0',0,2): #내일
+            j = value.text.replace('\t', '').replace(' ', '').replace('\n', '')
+            if i > len(temp)/2 and j.startswith('0', 0, 2):  # 내일
                 save.append(tomo_day+' '+j)
-            else: #오늘
+            else:  # 오늘
                 save.append(now_day+' '+j)
     elif option == 'PROGRAM':
         temp = soup.select('td.programInfo > div .program')
         for i, value in enumerate(temp):
-            j = value.text.replace('\t','').replace(' ','').replace('\n','')
+            j = value.text.replace('\t', '').replace(' ', '').replace('\n', '')
             save.append(j)
     return save
+
 
 def read_schedule(path):
     now_time = datetime.now().strftime("%H:%M")
@@ -36,34 +39,35 @@ def read_schedule(path):
     now_ocn_soup = read_Soup(path, now_day)
     tomo_ocn_soup = read_Soup(path, tomo_day)
 
-    airTime_ocn_list = []
-    airTime_ocn_list = cut_HTML(airTime_ocn_list, yest_ocn_soup, yest_day, now_day, 'TIME')
-    airTime_ocn_list = cut_HTML(airTime_ocn_list, now_ocn_soup, now_day, tomo_day, 'TIME')
-    airTime_ocn_list = cut_HTML(airTime_ocn_list, tomo_ocn_soup, tomo_day, tomo2_day, 'TIME')
+    temp1 = []
+    temp1 = cut_HTML(temp1, yest_ocn_soup, yest_day, now_day, 'TIME')
+    temp1 = cut_HTML(temp1, now_ocn_soup, now_day, tomo_day, 'TIME')
+    temp1 = cut_HTML(temp1, tomo_ocn_soup, tomo_day, tomo2_day, 'TIME')
 
-    program_ocn_list = []
-    program_ocn_list = cut_HTML(program_ocn_list, yest_ocn_soup, yest_day, now_day, 'PROGRAM')
-    program_ocn_list = cut_HTML(program_ocn_list, now_ocn_soup, now_day, tomo_day, 'PROGRAM')
-    program_ocn_list = cut_HTML(program_ocn_list, tomo_ocn_soup, tomo_day, tomo2_day, 'PROGRAM')
+    temp2 = []
+    temp2 = cut_HTML(temp2, yest_ocn_soup, yest_day, now_day, 'PROGRAM')
+    temp2 = cut_HTML(temp2, now_ocn_soup, now_day, tomo_day, 'PROGRAM')
+    temp2 = cut_HTML(temp2, tomo_ocn_soup, tomo_day, tomo2_day, 'PROGRAM')
 
-    start_index = [i for i, value in enumerate(airTime_ocn_list) if value >= (now_day+' '+now_time)][0]-1
-    airTime_ocn_list = airTime_ocn_list[start_index:start_index+4]
-    program_ocn_list = program_ocn_list[start_index:start_index+4]
-    return airTime_ocn_list, program_ocn_list
+    start_index = [i for i, value in enumerate(temp1)
+                    if value >= (now_day+' '+now_time)][0]-1
+    temp1 = temp1[start_index:start_index+4]
+    temp2 = temp2[start_index:start_index+4]
+
+    temp = np.array([temp1, temp2]).T
+    temp = pd.DataFrame(temp)
+    return temp
 
 
 if __name__ == '__main__':
     OCN_PATH = "http://ocn.tving.com/ocn/schedule?startDate="
-    airTime_ocn_list, program_ocn_list = read_schedule(OCN_PATH)
-    print(airTime_ocn_list)
-    print(program_ocn_list)
+    ocn_list = read_schedule(OCN_PATH)
+    print(ocn_list.to_html())
 
-    THRILLS_PATH = "http://ocnthrills.tving.com/ocnthrills/schedule?startDate="
-    airTime_ocnT_list, program_ocnT_list = read_schedule(THRILLS_PATH)
-    print(airTime_ocnT_list)
-    print(program_ocnT_list)
+    # THRILLS_PATH = "http://ocnthrills.tving.com/ocnthrills/schedule?startDate="
+    # ocnT_list = read_schedule(THRILLS_PATH)
+    # print(ocnT_list)
 
-    MOVIES_PATH = "http://ocnmovies.tving.com/ocnmovies/schedule?startDate="
-    airTime_ocnM_list, program_ocnM_list = read_schedule(MOVIES_PATH)
-    print(airTime_ocnM_list)
-    print(program_ocnM_list)
+    # MOVIES_PATH = "http://ocnmovies.tving.com/ocnmovies/schedule?startDate="
+    # ocnM_list = read_schedule(MOVIES_PATH)
+    # print(ocnM_list)
